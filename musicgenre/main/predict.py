50f0357a023a94f44fef2123e81835657f653095
DanielDominicSavioKennedy <saviodomnic2002@gmail.com>
2023-05-31 15:43:28 +0530

    step4:deploy the ML model in django

--- musicgenre/main/predict.py
+++ musicgenre/main/predict.py
@@ -1,13 +1,21 @@
-def predict_gen(meta1):
-    import pickle
-    import os
+import pickle
+import os
+
+def predict_genre(meta1):
+  
+    #pickle_file_path = './mlmodel.p'
+    #pickle_file_path = '../musicgenre/mlmodel.p'#os.path.join(os.getcwd(), 'mlmodel.p')
     from django.conf import settings
-    path = os.path.join(settings.MODELS, 'models.p')
-    with open(path, 'rb') as pickled:
-        data = pickle.load(pickled)
-    svmp = data['svmp']
-    norma = data['norma']
-    lgn = data['lgn']
-    x = norma.transform([meta1])
-    pred = svmp.predict(x)
-    return(lgn[pred[0]])
+    pickle_file_path = os.path.join(settings.MODELS, 'mlmodel.p')
+
+    # Load the pickle file
+    with open(pickle_file_path, 'rb') as f:
+        data = pickle.load(f)
+    scaler = data['scaler']
+    knn = data['knn']
+    encoder = data['encoder']
+    x = scaler.transform([meta1])
+    pred = knn.predict(x)
+    return(encoder.inverse_transform([pred]))
+
+#print(predict_genre([0.3501285, 0.08877166, 0.13018432, 0.00282838, 1784.1226409867395, 129745.48453872248, 2002.4124070990924, 85834.4103621946, 3805.7230301080335, 901252.916706492, 0.08304482066898686, 0.0007669456545940504, -4.90663e-05, 0.008172463, -1.0618483e-05, 0.005704438, 123.046875, 2569.3694, 295.8471, 235.58444, 151.03874, 167.99287, 89.17242, 67.60309, 69.001495, 82.21984, 63.346542, 61.76499, 51.280754, 41.215897, 40.51752, 49.784233, 52.424534, 36.535866, 41.60317, 55.053654, 46.941353]))
--- musicgenre/main/views.py
+++ musicgenre/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from .metadata import getdata
+from .predict import predict_genre
 # Create your views here.
 
 def main(request):
@@ -9,12 +10,14 @@ def main(request):
 def home(request):
     if request.method == 'POST':
         uploadfile = request.FILES['document']
-        print(uploadfile.name)
-        print(uploadfile.size)
+        #print(uploadfile.name)
+        #print(uploadfile.size)
         if not uploadfile.name.endswith('.wav'):
             messages.error(request,'Only .wav file type is allowed')
             return redirect(home)
         meta = getdata(uploadfile)
-        print(meta)
-    return render(request,'base.html')
+        pred=predict_genre(meta)
+        return render(request,'result.html',{"result":pred})
+    else:
+        return render(request,'base.html')
 
